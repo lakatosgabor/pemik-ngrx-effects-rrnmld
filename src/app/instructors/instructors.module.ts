fecb8725a3f2bfd09e89fac50e554d7773c8db8e
lakatosgabor <65727400+lakatosgabor@users.noreply.github.com>
2023-05-28 15:12:13 +0200

    1 file modified, 2 added

--- src/app/instructors/instructors.module.ts
+++ src/app/instructors/instructors.module.ts
@@ -4,13 +4,15 @@ import { InstructorsRoutingModule } from './instructors-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
-import * as fromEvents from './store/instructors.reducer';
+import * as fromInstructors from './store/instructors.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { InstructorEffects } from './store/instructor.effects';
 
 @NgModule({
   imports: [
     CommonModule, InstructorsRoutingModule, FormsModule, ReactiveFormsModule, RouterModule,
-    StoreModule.forFeature(fromEvents.eventsFeatureKey, fromEvents.eventsReducer),
-    EffectsModule.forFeature([EventEffects]),
+    StoreModule.forFeature(fromInstructors.instructorsFeatureKey, fromInstructors.instructorsReducer),
+    EffectsModule.forFeature([InstructorEffects]),
      MatButtonModule,
     MatCardModule,
     MatIconModule,
---
+++ src/app/instructors/store/instructor.effects.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { EMPTY } from 'rxjs';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { InstructorService } from '../../instructor.service';
+import { instructorLoadedAction, deleteInstructor } from './instructors.actions';
+
+@Injectable()
+export class InstructorEffects {
+
+  loadMovies$;
+
+  constructor(
+    private actions$: Actions,
+    private instructorService: InstructorService
+  ) {
+    this.loadMovies$ = createEffect(() => this.actions$.pipe(
+    ofType(InstructorActionTypes.instructorsRequested),
+    mergeMap(() => this.instructorService.getEvents()
+      .pipe(
+        map(instructors => (eventsLoadedAction({instructors}))),
+        catchError(() => EMPTY)
+      ))
+    )
+  );
+  }
+}
