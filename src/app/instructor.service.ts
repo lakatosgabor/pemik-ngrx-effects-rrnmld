e2562e3d1ebf0b9764e1e839fcf07c9d594f10d7
lakatosgabor <65727400+lakatosgabor@users.noreply.github.com>
2023-05-28 15:33:41 +0200

    1 file modified

--- src/app/instructor.service.ts
+++ src/app/instructor.service.ts
@@ -1,52 +1,52 @@
 import { Injectable } from '@angular/core';
-import { EventTable } from './event-list/events';
+import { InstructorTable } from './instructor-list/instructors';
 import { Observable } from 'rxjs';
 import { RequestService } from './request.service';
 import { HttpHeaders } from '@angular/common/http';
 import { map, debounceTime } from 'rxjs/operators';
 
-const EVENT_URL = 'api/events';
+const INSTRUCTOR_URL = 'api/instructors';
 
 @Injectable()
-export class EventService {
+export class InstructorService {
 
   constructor(private requestService: RequestService) { }
 
-  getEvents(): Observable<any> {
+  getInstructors(): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json'
       })
     };
-    return this.requestService.get<any>(EVENT_URL, httpOptions);
+    return this.requestService.get<any>(INSTRUCTOR_URL, httpOptions);
   }
 
-  getEvent(eventId): Observable<any>{
-    return this.requestService.get(`${EVENT_URL}/${eventId}`);
+  getInstructor(instructorId): Observable<any>{
+    return this.requestService.get(`${INSTRUCTOR_URL}/${instructorId}`);
   }
 
-  createEvent(event: any): Observable<any> {
-    return this.requestService.post(`${EVENT_URL}/`, event);
+  createInstructor(instructor: any): Observable<any> {
+    return this.requestService.post(`${INSTRUCTOR_URL}/`, instructor);
   }
 
-  updateEvent(event: any): Observable<any> {
-    return this.requestService.put(`${EVENT_URL}/`, event);
+  updateInstructor(instructor: any): Observable<any> {
+    return this.requestService.put(`${INSTRUCTOR_URL}/`, instructor);
   }
 
-  deleteEvent(eventId: number): Observable<any> {
+  deleteInstructor(instructorId: number): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json'
       })
     };
-    const url = `${EVENT_URL}/${eventId}`;
+    const url = `${INSTRUCTOR_URL}/${instructorId}`;
     return this.requestService.delete(url, httpOptions);
   }
 
-  eventNameExists(name: string): Observable<boolean> {
-    return this.getEvents().pipe(
-      map(events => {
-        return events.findIndex(event => event.name === name) !== -1;
+  instructorNameExists(name: string): Observable<boolean> {
+    return this.getInstructors().pipe(
+      map(instructors => {
+        return instructors.findIndex(instructor => instructor.name === name) !== -1;
       })
     );
   }
