fb0ce17124a50ed1c34a559d78851902c95cac2f
lakatosgabor <65727400+lakatosgabor@users.noreply.github.com>
2023-05-28 16:46:02 +0200

    1 file modified

--- src/app/instructors/instructors.module.ts
+++ src/app/instructors/instructors.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InstructorsRoutingModule } from './instructors-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { StoreModule } from '@ngrx/store';
-import * as fromInstructors from './store/instructors.reducer';
-import { EffectsModule } from '@ngrx/effects';
-import { InstructorEffects } from './store/instructor.effects';
+import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { StoreModule } from '@ngrx/store';
+
+import { HighlightDirective } from '../directives/highlight.directive';
+import { FirstLetterPipe } from '../pipes/first-letter.pipe';
+
+import { InstructorsRoutingModule } from './instructors-routing.module';
+import { InstructorsComponent } from './instructors.component';
+import { InstructorListComponent } from '../instructor-list/instructor-list.component';
+import { InstructorSocialComponent } from '../instructor-social/instructor-social.component';
+import { InstructorDetailsComponent } from '../instructor-details/instructor-details.component';
+import { InstructorService } from '../instructor.service';
+import { InstructorCreateComponent } from '../instructor-create/instructor-create.component';
+import { InstructorNameValidator } from '../validators/instructor-name.validator';
+import * as fromEvents from './store/instructors.reducer';
+import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { InstructorEffects } from './store/instructors.effects';
 
 @NgModule({
   imports: [
     CommonModule, InstructorsRoutingModule, FormsModule, ReactiveFormsModule, RouterModule,
-    StoreModule.forFeature(fromInstructors.instructorsFeatureKey, fromInstructors.instructorsReducer),
+    StoreModule.forFeature(fromEvents.instructorsFeatureKey, fromEvents.instructorsReducer),
     EffectsModule.forFeature([InstructorEffects]),
      MatButtonModule,
     MatCardModule,
@@ -24,6 +36,13 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatInputModule,
     MatProgressSpinnerModule
   ],
-  declarations: []
+  declarations: [
+    InstructorListComponent, InstructorSocialComponent,
+    InstructorDetailsComponent, InstructorCreateComponent,
+    InstructorsComponent, HighlightDirective, FirstLetterPipe
+  ],
+  providers: [
+    InstructorService, InstructorNameValidator
+  ]
 })
 export class InstructorsModule { }
