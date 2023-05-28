73406d291a47ad08f35fa0dd50911d9c6a73724b
lakatosgabor <65727400+lakatosgabor@users.noreply.github.com>
2023-05-28 16:28:04 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { EventsFeatureState } from './events/store/events.reducer';
+import { InMemoryInstructorService } from './in-memory-instructor.service';
 
 export interface AppState {
   eventsFeature: EventsFeatureState;
@@ -30,6 +31,7 @@ export interface AppState {
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, HttpClientModule,
   environment.isMockEnabled ? HttpClientInMemoryWebApiModule.forRoot(InMemoryEventService) : [],
+  environment.isMockEnabled ? HttpClientInMemoryWebApiModule.forRoot(InMemoryInstructorService) : [],
   StoreModule.forRoot({}),
   EffectsModule.forRoot(),
   AppRoutingModule,
@@ -41,6 +43,6 @@ export interface AppState {
     MatProgressSpinnerModule,],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [InMemoryEventService, RequestService, httpInterceptorProviders, AuthService]
+  providers: [InMemoryEventService, InMemoryInstructorService, RequestService, httpInterceptorProviders, AuthService]
 })
 export class AppModule { }
