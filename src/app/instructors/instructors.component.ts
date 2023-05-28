43df821e5309dd652759e1c925deb31aa6fd0f8b
lakatosgabor <65727400+lakatosgabor@users.noreply.github.com>
2023-05-28 14:39:38 +0200

    1 file modified, 5 added

---
+++ src/app/instructors/instructors.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './instructors.component.html',
+  styleUrls: ['./instructors.component.css']
+})
+export class InstructorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
--- src/app/instructors/instructors.module.ts
+++ src/app/instructors/instructors.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InstructorsRoutingModule } from './instructors-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, InstructorsRoutingModule, FormsModule, ReactiveFormsModule, RouterModule,
+    StoreModule.forFeature(fromEvents.eventsFeatureKey, fromEvents.eventsReducer),
+    EffectsModule.forFeature([EventEffects]),
+     MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   declarations: []
 })
