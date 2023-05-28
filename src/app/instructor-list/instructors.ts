9a61f45fe930eadd9531a12fd87de859d9daa9bc
lakatosgabor <65727400+lakatosgabor@users.noreply.github.com>
2023-05-28 15:53:01 +0200

    1 file added

---
+++ src/app/instructor-list/instructors.ts
@@ -0,0 +1,18 @@
+export class InstructorTable {
+  public static instructors = [
+    {
+      id: 1,
+      name: 'Gipsz Jakab',
+      neptun: 10000,
+      email: 'teszt@email.hu',
+      position: 'oktató'
+    },
+    {
+      id: 1,
+      name: 'Teszt ELek',
+      neptun: 20000,
+      email: 'teszt@email.hu',
+      position: 'oktató'
+    },
+  ];
+}
